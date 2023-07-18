eb4ddacb58960833df140c559b4d33dd43500d4d
Bob <masterledger@gmail.com>
2023-07-18 08:01:54 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -28,14 +28,3 @@ app.get("/api/comments", function(req, res) {
     res.json(JSON.parse(data));
   });
 });
-
-app.post("/api/comments", function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    var comments = JSON.parse(data);
-    var newComment = {
-      id: Date.now(),
